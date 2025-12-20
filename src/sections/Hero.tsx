0af53fc99cbfc7fa74c2b7d228329bb6b8ae44cf
Olaitandev <olaitan.dev@gmail.com>
2025-12-20 03:14:49 +0100

    m

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,7 +9,7 @@ import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip " >
+    <div className="relative z-0 py-32 md:py-48 lg:py-60 overflow-x-clip " >
       <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
         <div
           className="absolute inset-0 -z-30 opacity-5"
@@ -41,7 +41,7 @@ export const HeroSection = () => {
           <SparkleIcon className="size-5 text-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration="34s">
-          <div className="size-2 bg-emerald-300/20 rounded-full" />
+          <div className="rounded-full size-2 bg-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
           size={530}
@@ -74,7 +74,7 @@ export const HeroSection = () => {
           <StarIcon className="size-8 text-emerald-300" />
         </HeroOrbit>
         <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration="42s">
-          <div className="size-2 bg-emerald-300/20 rounded-full" />
+          <div className="rounded-full size-2 bg-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
           size={710}
@@ -87,7 +87,7 @@ export const HeroSection = () => {
           <SparkleIcon className="size-14 text-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit size={720} rotation={85} shouldOrbit orbitDuration="46s">
-          <div className="size-3 bg-emerald-300/20 rounded-full" />
+          <div className="rounded-full size-3 bg-emerald-300/20" />
         </HeroOrbit>
         <HeroOrbit
           size={800}
@@ -120,7 +120,7 @@ export const HeroSection = () => {
         </div>
 
         <div className="max-w-lg mx-auto">
-          <h1 className="font-serif text-3xl text-center mt-8 tracking-wide md:text-5xl ">
+          <h1 className="mt-8 font-serif text-3xl tracking-wide text-center md:text-5xl ">
             Hi, I&apos;m Michael. <br /> A Frontend Developer.
           </h1>
 
@@ -129,15 +129,15 @@ export const HeroSection = () => {
             appealing interfaces for web and mobile applications.
           </p>
         </div>
-        <div className="flex flex-col md:flex-row items-center mt-8 gap-4 justify-center">
-          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+        <div className="flex flex-col items-center justify-center gap-4 mt-8 md:flex-row">
+          <a href="#projects" className="inline-flex items-center h-12 gap-2 px-6 border border-white/15 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
-          </button>
-          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl ">
+          </a>
+          <a href="#contact" className="inline-flex items-center h-12 gap-2 px-6 text-gray-900 bg-white border border-white rounded-xl ">
             <span>👋</span>
             <span className="">Lets Connect</span>
-          </button>
+          </a>
         </div>
       </div>
     </div>
